c1cd3dffec96a8de4a385fd04ed9271c1cf2574f
Amardeep Sharma <amardeepsharma2907@gmail.com>
2025-07-07 17:30:33 +0530

    chnages in newarrievals and newtranding files and category and allproudtcs

--- Front-end/src/components/category/AllProducts.jsx
+++ Front-end/src/components/category/AllProducts.jsx
@@ -62,7 +62,7 @@ const AllProducts = ({ products }) => {
   };
 
   return (
-    <div className="max-w-8xl mx-5 px-4 py-10 font-sans bg-white min-h-screen">
+    <div className="max-w-8xl mx-5 px-4 py-5 font-sans bg-white min-h-screen">
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
         {products.map((product) => {
           const isWishlisted = wishlistIds.includes(product._id);
@@ -70,7 +70,7 @@ const AllProducts = ({ products }) => {
           return (
             <div
               key={product._id}
-              className="relative bg-white overflow-hidden transition-all shadow-md group"
+              className="relative  bg-white overflow-hidden transition-all  group"
             >
               <div className="relative overflow-hidden w-full h-44 sm:h-52 md:h-90 group">
                 <Link to={`/product/${product._id}`}>
@@ -89,45 +89,34 @@ const AllProducts = ({ products }) => {
                 <div className="absolute top-1/2 right-4 -translate-y-1/2 flex flex-col items-center space-y-2 z-10">
                   <button
                     onClick={() => {
+                      addToCart(product);
+                      toast.success("Added to Cart");
+                    }}
+                  className="bg-white w-12 h-12 flex items-center justify-center rounded-full shadow hover:bg-red-500 text-gray-600 hover:text-white transition opacity-100 lg:opacity-0 lg:translate-x-4 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 duration-300 delay-100">
+                 <ShoppingCart size={20} />
+                  </button>
+                  <button
+                    onClick={() => {
                       setSelectedProduct(product);
                       setShowQuickView(true);
                     }}
-                    className="bg-white w-10 h-10 flex items-center justify-center rounded-full shadow hover:bg-red-500 text-gray-600 hover:text-white transition 
-                      transform opacity-100 lg:opacity-0 lg:translate-x-4 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 duration-300 delay-100"
+                    className="bg-white w-12 h-12  flex items-center justify-center rounded-full shadow hover:bg-red-500 text-gray-600 hover:text-white transition 
+                      transform opacity-100 lg:opacity-0 lg:translate-x-4 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 duration-300 delay-200"
                   >
-                    <Eye size={18} />
+                    <Eye size={20} />
                   </button>
 
                   <button
                     onClick={() => toggleWishlist(product)}
-                    className={`w-10 h-10 flex items-center justify-center rounded-full shadow transition 
-                      transform opacity-100 lg:opacity-0 lg:translate-x-4 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 duration-300 delay-200 
+                    className={`w-12 h-12  flex items-center justify-center rounded-full shadow transition 
+                      transform opacity-100 lg:opacity-0 lg:translate-x-4 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 duration-300 delay-300 
                       ${
                         isWishlisted
                           ? "bg-red-500 text-white"
                           : "bg-white text-gray-600 hover:bg-red-500 hover:text-white"
                       }`}
                   >
-                    <Heart size={16} />
-                  </button>
-
-                  <button
-                    className="bg-white w-10 h-10 flex items-center justify-center rounded-full shadow hover:bg-red-500 text-gray-600 hover:text-white transition 
-                      transform opacity-100 lg:opacity-0 lg:translate-x-4 lg:group-hover:opacity-100 lg:group-hover:translate-x-0 duration-300 delay-300"
-                  >
-                    <RefreshCcw size={16} />
-                  </button>
-                </div>
-
-                {/* Add to Cart */}
-                <div className="absolute bottom-0 left-0 mb-2 w-full flex justify-center 
-                  opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300 z-10">
-                  <button
-                    onClick={() => handleAddToCart(product)}
-                    className="w-[90%] h-10 sm:h-12 relative overflow-hidden px-2 py-2 text-white font-bold z-10 bg-[#d75a3c] group/button"
-                  >
-                    <span className="absolute inset-0 bg-white transition-all duration-500 ease-out transform -translate-x-full group-hover/button:translate-x-0 z-0"></span>
-                    <span className="relative text-black z-10">Add to Cart</span>
+                    <Heart size={20} />
                   </button>
                 </div>
               </div>
@@ -137,9 +126,11 @@ const AllProducts = ({ products }) => {
                 <p className="text-xs uppercase text-gray-400 tracking-widest">
                   {product.category || "Handmade"}
                 </p>
-                <h2 className="text-sm font-semibold text-gray-800 truncate">
-                  {product.title}
-                </h2>
+                 <Link to={`/product/${product._id}`}>
+                           <h2 className="inline-block text-sm py-3 font-semibold text-gray-800 truncate hover:text-red-500 cursor-pointer">
+                             {product.title}
+                           </h2>
+                         </Link>
                 <p className="text-sm font-medium text-gray-900">
                   ₹{product.price}
                 </p>
